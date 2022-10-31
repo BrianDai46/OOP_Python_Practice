725f05aa1e22007d35f9a0da417e4423c8aebe5b
BrianDai <2687126832@qq.com>
2022-10-31 15:34:01 -0400

    First commit

---
+++ App 1 Geometry Game - Inheritance/main.py
@@ -0,0 +1,16 @@
+import turtle
+# create a canvas
+myturtle = turtle.Turtle()
+
+myturtle.penup()
+myturtle.goto(50, 70)
+myturtle.pendown()
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(100)
+
+turtle.done()
